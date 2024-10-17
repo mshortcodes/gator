ec9edca83cad6b71616b1a7c84f6abca34610aa6
mshortcodes <mshortcodes@gmail.com>
2024-10-16 18:20:00 -0700

    update aggregate handler

--- handler_feeds_agg.go
+++ handler_feeds_agg.go
@@ -3,15 +3,48 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"time"
 )
 
 func handlerAgg(s *state, cmd command) error {
-	url := "https://www.wagslane.dev/index.xml"
-	rssFeed, err := fetchFeed(context.Background(), url)
-	if err != nil {
-		return fmt.Errorf("couldn't fetch feed: %v", err)
+	if len(cmd.args) != 1 {
+		return fmt.Errorf("need exactly one arg")
 	}
 
-	fmt.Printf("Feed: %+v\n", rssFeed)
+	timeBetweenReqs, err := time.ParseDuration(cmd.args[0])
+	if err != nil {
+		return fmt.Errorf("invalid duration: %v", err)
+	}
+
+	fmt.Printf("collecting feeds every %s...\n", timeBetweenReqs)
+	ticker := time.NewTicker(timeBetweenReqs)
+	for ; ; <-ticker.C {
+		scrapeFeeds(s)
+	}
+}
+
+func scrapeFeeds(s *state) error {
+	feed, err := s.db.GetNextFeedToFetch(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't get next feed to fetch: %v", err)
+	}
+
+	log.Println("Found a feed to fetch!")
+
+	_, err = s.db.MarkFeedFetched(context.Background(), feed.ID)
+	if err != nil {
+		return fmt.Errorf("couldn't mark feed")
+	}
+
+	rssFeed, err := fetchFeed(context.Background(), feed.Url)
+	if err != nil {
+		return fmt.Errorf("couldn't fetch RSS feed")
+	}
+
+	for _, item := range rssFeed.Channel.Item {
+		fmt.Printf("%s\n", item.Title)
+	}
+
 	return nil
 }
